227b1fbcf855b2538b5eaf880c4d10f3490ddfd5
Matheus Alves <105603566+MamoruSanCD@users.noreply.github.com>
2024-08-11 05:27:08 -0300

    Add files via upload

--- auto_clear.sh
+++ auto_clear.sh
@@ -1,2 +1,2 @@
 read -p "A cada quantos segundos? " temp_loop;
-sudo -i watch -n $temp_loop "free -h; rm -r /tmp/; rm -r /var/tmp/; rm -r /var/cache/; rm -r /var/backup/; sudo apt autoclean; sudo apt autoremove; sudo apt update; sudo apt upgrade; /proc/sys/vm/drop_caches; swapoff -a; swapon -a; free -h "
+sudo -i watch -n $temp_loop "free -h; rm -r /tmp/; rm -r /var/tmp/;; rm -r /var/backup/; sudo apt autoclean; sudo apt autoremove; sudo apt update; sudo apt upgrade; /proc/sys/vm/drop_caches; swapoff -a; swapon -a; free -h "
