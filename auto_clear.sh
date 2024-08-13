733d7c1f46f12886034e063624f7dc5f0a9394a3
striker765 <Thiago-carvalho789@outlook.com>
2024-08-13 02:25:53 -0300

    Organização clean code / reformulação remdmi.md

--- auto_clear.sh
+++ auto_clear.sh
@@ -1,2 +1,25 @@
-read -p "A cada quantos segundos? " temp_loop;
-sudo -i watch -n $temp_loop "free -h -l -t; sync; echo 1 > /proc/sys/vm/drop_caches; sync; echo 2 > /proc/sys/vm/drop_caches; sync; echo 3 > /proc/sys/vm/drop_caches; swapoff -a; swapon -a; rm -r /temp/*"
+#!/bin/bash
+
+
+read -p "A cada quantos segundos? " temp_loop
+
+while true; do
+    # Exibe informações sobre a memória
+    free -h -l -t
+    
+    # Sincroniza dados
+    sync
+    
+    # Limpa caches
+    echo 1 > /proc/sys/vm/drop_caches
+    sync
+    echo 2 > /proc/sys/vm/drop_caches
+    sync
+    echo 3 > /proc/sys/vm/drop_caches
+    
+    # Remove conteúdo dos diretórios especificados
+    rm -rf /temp/*
+    
+    # Aguarda o intervalo especificado
+    sleep $temp_loop
+done
