610ef85b4b1533b2acf391a4e3dd686f75612072
Matheus Alves <105603566+MamoruSanCD@users.noreply.github.com>
2024-11-03 03:34:07 -0300

    Update auto_clear.sh - Definição de Gateway
    #Atualização – 3.0
    Nessa atualização, passa ser possível alterar o gateway da rede caso tenha mais de um acesso à internet, por se tratar de algo voltado a mim, coloquei alguns dos tipos de acessos que tenho, mas nada impede que sejam adicionados outros por vocês mesmos.

--- auto_clear.sh
+++ auto_clear.sh
@@ -1,2 +1,27 @@
-read -p "A cada quantos segundos? " temp_loop;
-sudo -i watch -n $temp_loop "free -h; rm -r /tmp/; rm -r /var/tmp/; rm -r /var/cache/; rm -r /var/backup/; sudo apt autoclean; sudo apt autoremove; sudo apt update; sudo apt upgrade; /proc/sys/vm/drop_caches; swapoff -a; swapon -a; free -h "
+@@ -1,5 +1,21 @@
+# Definindo o host para tráfego não direcionado
+sudo ip route add default via < Seu host > dev enx5c531073e79c
+read -p " Qual é o gateway?: " route_ip ;
+
+read -p " Qual o tipo de rede? 1- wlp2s0, 2- enx5c531073e79c, 3- tun0 ou 4- tun1? " opt_web ;
+
+if [[ " $opt_web "  -eq 1 ]] ;  então   # Note as aspas duplas e o espaço correto
+    tipo_web= " wlp2s0 "
+elif [[ " $opt_web "  -eq 2 ]] ;  então
+    tipo_web= " enx5c531073e79c "
+elif [[ " $opt_web "  -eq 3 ]] ;  então
+    tipo_web= " tun0 "
+elif [[ " $opt_web "  -eq 4 ]] ;  então
+    tipo_web= " tun1 "
+outro
+    echo  " Resposta inválida "
+sim
+
+sudo ip route add default via $route_ip dev $web_type
+
+# Definição do timer de limpeza da RAM
+read -p " A cada quantos segundos? " temp_loop ;
+@@ -24,18 +40,3 @@ sudo du -h /var | classificar -rh | cabeça -n 10
+
+# Manuteção da RAM e do diretório /temp/
+sudo -i watch -n $temp_loop  " livre -h -l -t; sincronizar; eco 1 > /proc/sys/vm/drop_caches; sincronizar; eco 2 > /proc/sys/vm/drop_caches; sincronizar; eco 3 > /proc/sys/vm/drop_caches; swapoff -a; swapon -a; rmdir -p /tmp/*; rm -r /tmp/* "
